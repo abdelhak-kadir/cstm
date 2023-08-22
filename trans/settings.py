5be7d4c009a21bf4c0039c9ef2a430dbc252bfeb
abdelhak <abdelhakaitkadir03@gmail.com>
2023-08-22 09:50:27 +0100

    add on setting

--- trans/settings.py
+++ trans/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('MY_DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['162.243.188.66']
 
 
 # Application definition
